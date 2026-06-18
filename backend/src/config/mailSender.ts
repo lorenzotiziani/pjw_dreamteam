d35bf7c1441f8d3a0da469ca792a481721ddb643
lorenzotiziani <lorenzo.tiziani04@gmail.com>
2026-06-18 09:43:20 +0200

    Update mailSender.ts

--- backend/src/config/mailSender.ts
+++ backend/src/config/mailSender.ts
@@ -1,9 +1,10 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
+  service: "gmail",
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
